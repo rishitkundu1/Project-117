03b61e3d6547f0e0623e0b13d82bb34d51179325
rishitkundu1 <85060575+rishitkundu1@users.noreply.github.com>
2022-08-22 20:01:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ function setup(){
       canvas = createCanvas(280, 280);
       canvas.center();
       canvas.background("white");
+
+      canvas.mouseReleased(classifyCanvas);
 }
 function draw(){
       check_sketch();
@@ -42,4 +44,26 @@ function check_sketch(){
             answer_holder = "";
             updateCanvas();
       }
+}
+function preload(){
+      classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+      strokeWeight(13);
+      stroke("blue");
+      if(mouseIsPressed){
+            line(pmouseX, pmouseY, mouseX, mouseY)
+      }
+}
+function classifyCanvas(){
+      classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results){
+      if(error){
+            console.error();
+      }
+      console.log(results);
+      drawn_sketch = results[0].label;
+      document.getElementById("sketchStatus").innerHTML = "Your Sketch " + drawn_sketch;
+      document.getElementById("cofidence").innerHTML = "Confidence : " + Math.round(results[0].confidence * 100);
 }
